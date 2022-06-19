16e5342751e8f35c2f2891c5479fffe50e74ba1b
qiuqi88 <2436233790@qq.com>
2022-06-19 18:06:39 +0800

    添加文件

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author qiuqi
+ * @dote 2022/6/19 - 17:52
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
